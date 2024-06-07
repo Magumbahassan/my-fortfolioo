34ecbce94721f2984f0fa0c04dd89343b7d1ea73
Magumbahassan <149811118+Magumbahassan@users.noreply.github.com>
2024-06-07 14:47:31 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Skill Icons
+const skillicons = document.getElementsByTagName('i');
+const infoBox = document.getElementById('infoBox');
+
+
+for(const icon of skillicons){
+    icon.addEventListener('mouseenter', function(e){
+        icon.style.color='blueviolet';
+    } ) 
+    icon.addEventListener('mouseleave', function(e){
+        icon.style.color='black';
+    } ) 
+}
+
+// Progress Bar
+
+    const progresses = document.getElementsByClassName('progress_done');
+    
+    for(const progress of progresses){
+        setTimeout(()=>{
+        progress.style.width = progress.getAttribute('data-done')+'%';
+        }, 500)
+    }
+
+
+
+
+    
